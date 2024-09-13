e35260fb8c0bbe6a78aa71d939de5d6bf3060a3f
Angelo <jyhhmj38@gmail.com>
2024-09-13 11:45:38 +0900

    init

--- src/CH07/C01WHILE.java
+++ src/CH07/C01WHILE.java
@@ -5,23 +5,22 @@ import java.util.Scanner;
 public class C01WHILE {
 
 	public static void main(String[] args) {
-		
+
 		// 01 while 기본
 		// while (조건식) {
-		//  조건식이 참인 경우 실행되는 종속 문장 ( 1 개의 행일 때 생략 가능 )
-		//}
-		
-		
+		// 조건식이 참인 경우 실행되는 종속 문장 ( 1 개의 행일 때 생략 가능 )
+		// }
+
 		// 탈출용 변수
 		// 탈출을 위한 조건식
 		// 탈출을 위한 연산식
-		
+
 //		int i = 0;
 //		while(i<5) {
 //			System.out.println("HELLO WORLD");
 //			i++;
 //		}		
-		
+
 		// 02 1 - 10 까지 합
 //		int i = 1;
 //		int sum = 0;
@@ -32,8 +31,8 @@ public class C01WHILE {
 //		}
 //		System.out.println("sum : " + sum);
 //	}
-		//01 1 - N 까지의 합
-		
+		// 01 1 - N 까지의 합
+
 //		Scanner sc = new Scanner(System.in);
 //		int n = sc.nextInt();
 //		
@@ -45,9 +44,9 @@ public class C01WHILE {
 //			i++;
 //		}
 //		System.out.printf("%d 부터 %d 까지의 합 : %d\n", 1 ,n, sum);
-		
-		//02 N - M 까지의 합을 구합니다. ( N < M )
-		
+
+		// 02 N - M 까지의 합을 구합니다. ( N < M )
+
 //		Scanner sc = new Scanner(System.in);
 //		int n = sc.nextInt();
 //		int m = sc.nextInt();
@@ -76,10 +75,9 @@ public class C01WHILE {
 //			sum += i;
 //			i++;
 //		}
-		
-		
-		//03 01 1 - N 까지 수중에 짝수 / 홀수 의 합을 각각 출력
-		
+
+		// 03 01 1 - N 까지 수중에 짝수 / 홀수 의 합을 각각 출력
+
 //		Scanner sc = new Scanner(System.in);
 //		int n = sc.nextInt();
 //		int i = 1;
@@ -95,16 +93,11 @@ public class C01WHILE {
 //			}
 //			i++;
 //		}
-		
-		
-		
-		//04 1 - N 까지 수중에 3의 배수만 출력하고 그 합도 출력
-		
-		
-		
-		//05 1 - N 까지 수중에 4의 배수를 출력하고 4의 배수가 아닌 나머지의 합을 구하세요
 
-		
+		// 04 1 - N 까지 수중에 3의 배수만 출력하고 그 합도 출력
+
+		// 05 1 - N 까지 수중에 4의 배수를 출력하고 4의 배수가 아닌 나머지의 합을 구하세요
+
 		// 구구단 2 단
 //		int i = 2;
 //		int j = 1;
@@ -116,7 +109,7 @@ public class C01WHILE {
 //		}
 //		
 		// 구구단 2 ~ 9 단
-		
+
 //		int i = 2;	
 //		int j = 1;
 //		while ( i <= 9) {
@@ -129,5 +122,43 @@ public class C01WHILE {
 //			i++;
 //		}
 //		
+
+		// 9단 부터 2단
+
+//		int i = 9;	
+//		int j = 1;
+//		while ( i >= 2) {
+//			while( j <= 9 ){
+//				System.out.println(i + "X" + j + " = " + (i*j) );
+//				j++;
+//			}
+//			System.out.println();
+//			j = 1;
+//			i--;
+//		}
+//		
+
+		// 2단 부터 n단
+
+		Scanner sc = new Scanner(System.in);
+		System.out.print("단수 입력 : ");
+		int n = sc.nextInt();
+
+		if (n >= 10) {
+			System.out.println("9 단 까지만 입력이 가능 합니다");
+			System.exit(n);
+		}
+
+		int i = 2;
+		int j = 1;
+		while (i <= n) {
+			while (j <= 9) {
+				System.out.println(i + "X" + j + " = " + (i * j));
+				j++;
+			}
+			System.out.println();
+			j = 1;
+			i++;
+		}
 	}
 }
---
+++ src/CH07/C02WHILE.java
@@ -0,0 +1,10 @@
+package CH07;
+
+public class C02WHILE {
+
+	public static void main(String[] args) {
+		
+		// 2단 - 9단
+	}
+
+}
