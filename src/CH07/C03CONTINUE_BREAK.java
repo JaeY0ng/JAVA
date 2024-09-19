da5ab62628a4d1dabdba423177e38fbeef219e44
Angelo <jyhhmj38@gmail.com>
2024-09-19 09:41:12 +0900

    init-

--- src/CH07/C03CONTINUE_BREAK.java
+++ src/CH07/C03CONTINUE_BREAK.java
@@ -10,19 +10,49 @@ public class C03CONTINUE_BREAK {
 //       BREAK
 		
 		// -1 을 입력하기 전까지 계속 정수값을 받아 누적 합 계산
-		Scanner sc = new Scanner(System.in);
-		int n;
-		int sum = 0;
-		while(true) {
-			n = sc.nextInt();
-			if(n == -1)
-				break;
-			sum+=n;
-		}
-		System.out.println("sum : " + sum);
+//		Scanner sc = new Scanner(System.in);
+//		int n;
+//		int sum = 0;
+//		while(true) {
+//			n = sc.nextInt();
+//			if(n == -1)
+//				break;
+//			sum+=n;
+//		}
+//		System.out.println("sum : " + sum);
+//		
 		
+		// 전체 구구단 출력 ( 7 x 7 = 49 까지만 출력 )
+//		int dan = 2;
+//		while(dan <= 9) {
+//			int i = 1;
+//			while(i <= 9) {
+//				System.out.printf("%d x %d = %d\n", dan , i , dan * i);
+//				i++;
+//			}
+//			System.out.println();
+//			dan++;
+//		}
+//		
+//		
 
 //       CONTINUE
+		
+		// 1 부터 10 까지의 수 중에 3 의 배수를 제외한 합 출력
+		
+		int i = 1;
+		int sum = 0;
+		while( i <= 10 ) {
+			
+			if( i % 3 == 0) {
+				i++;
+				continue;
+			}
+			System.out.println("i : " + i);
+			sum += i;
+			i++;
+		}
+		System.out.println("sum : " + sum);
 	}
 
 }
---
+++ src/CH07/C04FOR.java
@@ -0,0 +1,27 @@
+package CH07;
+
+public class C04FOR {
+
+	public static void main(String[] args) {
+		
+		// FOR 문 기본 - 1 부터 10 까지 합
+		
+//		int i = 0;  // 탈출용 변수
+//		while ( i < 5 ) { // 탈출을 위한 조건식
+//			System.out.println("HELLO WORLD");
+//			i++; 		
+//			}
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("HELLO WORLD");
+		}
+		
+		// 1 부터 -10 까지 합
+		// 1 부터 -N 까지 합
+		// N 부터 -M 까지 합
+		// 2단 찍기
+		// N단 찍기
+		// 별찍기
+	}
+
+}
