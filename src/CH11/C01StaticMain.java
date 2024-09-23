97fca2664bd948d1b3379f8bf2e9da9e7feb3a36
Angelo <jyhhmj38@gmail.com>
2024-09-23 16:41:07 +0900

    .

--- src/CH11/C01StaticMain.java
+++ src/CH11/C01StaticMain.java
@@ -1,8 +1,8 @@
 package CH11;
 
 class C01Simple{
-	static int num1;
-	int num2;
+	static int num1; // 공유 메모리 영역 ( 클래스 , 메서드 영역 ) 에 할당
+	int num2;		 // 객체 생성과는 무관하게 사용가능 ( 클래스명.static 변수명)
 	
 	C01Simple(){
 		this.num1 = 0;
@@ -13,12 +13,12 @@ class C01Simple{
 	public String toString() {
 		return "C01Simple [num2=" + num2 + "]";
 	}
-	
 }
 
 
 public class C01StaticMain {
 	public static void main(String[] args) {
-		
+		C01Simple.num1 = 100;
+		System.out.println("num1 : " + C01Simple.num1);
 	}
 }
