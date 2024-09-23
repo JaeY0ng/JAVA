7de217e4bac88de7b691913c9178bffeac18402d
Angelo <jyhhmj38@gmail.com>
2024-09-23 11:31:52 +0900

    .

--- src/CH09/C06StringClassMain.java
+++ src/CH09/C06StringClassMain.java
@@ -1,6 +1,6 @@
 package CH09;
 
-import CH01.C01SystemOut;
+import java.util.Scanner;
 
 public class C06StringClassMain {
 
@@ -23,11 +23,24 @@ public class C06StringClassMain {
 		
 	   //StringBuilder , StringBuffer
 	    StringBuffer str5 = new StringBuffer();
-	    System.out.println("------------------");
+	    System.out.println("----------------------");
 	    System.out.printf("%x\n", System.identityHashCode(str5));
 	    str5.append(str1).append(str2).append(str3).append(str4);
 	    System.out.printf("%x\n", System.identityHashCode(str5));
+	    System.out.println(str5);
 	    System.out.println("----------------------");
+	    
+	    System.out.println("문자열 길이 : " + (str1.length()));
+	    System.out.println("문자 찾기 : " + (str1.charAt(1)));
+//	    Scanner sc = new Scanner (System.in);
+//	    char ch = sc.next().charAt(1);
+//	    System.out.println("ch : " + ch);
+	    
+	    System.out.println("문자찾기 : " + (str1.indexOf("a")));
+	    System.out.println("문자찾기 : " + (str1.lastIndexOf("a")));
+	    
+	    System.out.println("문자 포함여부 : " + (str1.contains("va")));
+	    System.out.println("문자 포함여부 : " + (str1.contains("abs")));
 	}
 
 }
