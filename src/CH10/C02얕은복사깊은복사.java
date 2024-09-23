2fca230f24d39632296ed231bfa91139a740711a
Angelo <jyhhmj38@gmail.com>
2024-09-23 15:28:42 +0900

    .

--- src/CH10/C02얕은복사깊은복사.java
+++ src/CH10/C02얕은복사깊은복사.java
@@ -1,6 +1,8 @@
 package CH10;
 
-public class C01얕은복사깊은복사 {
+import java.util.Arrays;
+
+public class C02얕은복사깊은복사 {
 
 	public static void main(String[] args) {
 		// 얕은복사 ( 주소 복사 )
@@ -20,8 +22,13 @@ public class C01얕은복사깊은복사 {
 		for (int i = 0; i < arr3.length; i++) {
 			arr3[i] = arr1[i];
 		}
+		int arr4[] = Arrays.copyOf(arr1, arr1.length);
 		System.out.println("arr1 : " + arr1);
+		System.out.println("arr2 : " + arr2);
 		System.out.println("arr3 : " + arr3);
+		System.out.println("arr4 : " + arr4);
+		
+		Arrays.stream(arr4).forEach(el->System.out.print(el+ " "));
 	}
 
 }
---
+++ src/CH10/C03다차원배열.java
@@ -0,0 +1,11 @@
+package CH10;
+
+public class C03다차원배열 {
+
+	public static void main(String[] args) {
+
+		
+		
+	}
+
+}
