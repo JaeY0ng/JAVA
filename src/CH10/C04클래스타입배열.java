4d62ff8e4f9e82c2a40658782300423388fb4f74
Angelo <jyhhmj38@gmail.com>
2024-09-23 15:44:53 +0900

    .

---
+++ src/CH10/C04클래스타입배열.java
@@ -0,0 +1,27 @@
+package CH10;
+
+class C04Person {
+	String name;
+	int age;
+
+	public C04Person() {
+	}
+
+	public C04Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	@Override
+	public String toString() {
+		return "C04Person [name=" + name + ", age=" + age + "]";
+	}
+
+public class C04클래스타입배열 {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
