54fcaa9e471ac229613e162663f5b7dc9f1750f4
Angelo <jyhhmj38@gmail.com>
2024-09-23 16:37:00 +0900

    .

--- src/CH10/C04클래스타입배열.java
+++ src/CH10/C04클래스타입배열.java
@@ -4,8 +4,7 @@ class C04Person {
 	String name;
 	int age;
 
-	public C04Person() {
-	}
+	public C04Person() {}
 
 	public C04Person(String name, int age) {
 		super();
@@ -23,12 +22,15 @@ public class C04클래스타입배열 {
 
 	public static void main(String[] args) {
 		C04Person list[] = new C04Person[3];
+		list[0] = new C04Person();
 		list[0].name = "홍길동";
 		list[0].age = 55;
 
+		list[1] = new C04Person();
 		list[1].name = "김영희";
 		list[1].age = 35;
 
+		list[2] = new C04Person();
 		list[2].name = "김범수";
 		list[2].age = 40;
 
---
+++ src/CH11/C01StaticMain.java
@@ -0,0 +1,24 @@
+package CH11;
+
+class C01Simple{
+	static int num1;
+	int num2;
+	
+	C01Simple(){
+		this.num1 = 0;
+		this.num2 = 0;
+	}
+
+	@Override
+	public String toString() {
+		return "C01Simple [num2=" + num2 + "]";
+	}
+	
+}
+
+
+public class C01StaticMain {
+	public static void main(String[] args) {
+		
+	}
+}
