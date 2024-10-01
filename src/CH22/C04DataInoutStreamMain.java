777e79431d5d5fe78e585e9b37b8432963c11a19
Angelo <jyhhmj38@gmail.com>
2024-10-01 12:29:10 +0900

    .

--- src/CH22/C04DataInoutStreamMain.java
+++ src/CH22/C04DataInoutStreamMain.java
@@ -1,18 +1,31 @@
 package CH22;
 
-import java.io.DataOutputStream;
-import java.io.FileOutputStream;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class C04DataInoutStreamMain {
 
 	public static void main(String[] args) throws Exception {
-		FileOutputStream out = new FileOutputStream("C:\\TMP_IO\\test3.txt");
-		DataOutputStream dout = new DataOutputStream(out);
-		dout.writeUTF("홍길동");
-		dout.writeDouble(95.5);
-		dout.writeInt(100);
-		dout.flush();
-		dout.close();
+//		FileOutputStream out = new FileOutputStream("C:\\TMP_IO\\test3.txt");
+//		DataOutputStream dout = new DataOutputStream(out);
+//		dout.writeUTF("홍길동");
+//		dout.writeDouble(95.5);
+//		dout.writeInt(100);
+//		dout.flush();
+//		dout.close();
+		
+		FileInputStream in = new FileInputStream("C:\\TMP_IO\\text3.txt");
+		DataInputStream din = new DataInputStream(in);
+		String name = din.readUTF();
+		System.out.println(name);
+		double weight = din.readDouble();
+		System.out.println(weight);
+		int data = din.readInt();
+		System.out.println(data);
+		
+		
+		
 	}
 
 }
