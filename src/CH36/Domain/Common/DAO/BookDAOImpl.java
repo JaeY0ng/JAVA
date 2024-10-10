3532de837d61bf12bfee56e584d371b7098ed9ff
Angelo <jyhhmj38@gmail.com>
2024-10-10 11:13:49 +0900

    .

--- src/CH36/Domain/Common/DAO/BookDAOImpl.java
+++ src/CH36/Domain/Common/DAO/BookDAOImpl.java
@@ -5,6 +5,10 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import CH36.Domain.Common.DTO.BookDTO;
 
 public class BookDAOImpl {
 	
@@ -12,11 +16,12 @@ public class BookDAOImpl {
 	//DBCONN 속성
 	private String id = "root";
 	private String pw = "1234";
-	private String url = "jdbc:mysql://localhost:3306/bookDB";
+	private String url = "jdbc:mysql://localhost:3306/bookdb";
 	
 	private Connection conn;
 	private PreparedStatement pstmt;
 	private ResultSet rs;
+	
 	
 	
 	// 싱글톤 패턴 처리코드
@@ -36,6 +41,71 @@ public class BookDAOImpl {
 	}
 	
 	//CRUD
+	public List<BookDTO> select() throws Exception{
+		
+		List<BookDTO> list = new ArrayList();
+		
+		pstmt = conn.prepareStatement("select * from tbl_book");
+		
+		rs = pstmt.executeQuery();
+		BookDTO dto = null;
+		if(rs!=null) {
+			while(rs.next()) {
+				dto = new BookDTO();
+				dto.setBookCode(rs.getInt("bookCode"));
+				dto.setBookName(rs.getString("bookName"));
+				dto.setPublisher(rs.getString("publisher"));
+				dto.setIsbn(rs.getString("isbn"));
+				
+				list.add(dto);
+				
+			}
+		}
+		
+		return list;
+	}
+	public List<BookDTO> select(int bookCode){
+		return null;
+	}
+	public int insert(BookDTO dto) throws Exception {
+		pstmt = conn.prepareStatement("insert into tbl_book values(?,?,?,?)");
+		pstmt.setInt(1, dto.getBookCode());
+		pstmt.setString(2, dto.getBookName());
+		pstmt.setString(3, dto.getPublisher());
+		pstmt.setString(4, dto.getIsbn());
+		
+		int result = pstmt.executeUpdate();
+				
+		// 자원제거
+		pstmt.close();
+		
+		return result;
+	}
+	public int update(BookDTO dto) throws Exception{
+		pstmt = conn.prepareStatement("update tbl_book set BookName=?,Publisher=?,Isbn=? where BookCode=?");
+		pstmt.setString(1, dto.getBookName());
+		pstmt.setString(2, dto.getPublisher());
+		pstmt.setString(3, dto.getIsbn());
+		pstmt.setInt(4, dto.getBookCode());
+		
+		int result = pstmt.executeUpdate();
+				
+		// 자원제거
+		pstmt.close();
+		
+		return result;
+	}
+	public int delete(int bookCode) throws Exception {
+		pstmt = conn.prepareStatement("delete from tbl_book where bookCode=?");
+		pstmt.setInt(1,bookCode);
+		
+		int result = pstmt.executeUpdate();
+				
+		// 자원제거
+		pstmt.close();
+		
+		return result;
+	}
 	
 	
 }
--- src/CH36/Test/DaoTests.java
+++ src/CH36/Test/DaoTests.java
@@ -1,10 +1,9 @@
 package CH36.Test;
 
-import java.sql.SQLException;
-
 import org.junit.jupiter.api.Test;
 
 import CH36.Domain.Common.DAO.BookDAOImpl;
+import CH36.Domain.Common.DTO.BookDTO;
 
 
 class DaoTests {
@@ -14,5 +13,19 @@ class DaoTests {
 		BookDAOImpl dao = BookDAOImpl.getInstance();
 		
 	}
-
+	@Test
+	void BookDaoInsert() throws Exception{
+		BookDAOImpl dao = BookDAOImpl.getInstance();
+		dao.insert(new BookDTO(3, "이것이리눅스다","한빛미디어","111-111"));
+	}
+	@Test
+	void BookDaoUpdate() throws Exception{
+		BookDAOImpl dao = BookDAOImpl.getInstance();
+		dao.update(new BookDTO(3,"이것이자바다","혼빛미디어","222-222"));
+	}
+	@Test
+	void BookDaoDelete() throws Exception{
+		BookDAOImpl dao = BookDAOImpl.getInstance();
+		dao.delete(1);
+	}
 }
